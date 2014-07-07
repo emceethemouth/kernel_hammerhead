ab2e8353e707eaeb335a32f384fe699ac9d309c2
kiko <jfdsmabalot@gmail.com>
2014-07-07 13:58:53 +0800

    Point to GCC 4.9 Signed-off-by: Jean Francis Dominique S. Mabalot <jfdsmabalot@redhat.com>

--- kernel-build.sh
+++ kernel-build.sh
@@ -22,7 +22,7 @@ git checkout mainstream
 make clean
 make mrproper
 export ARCH=arm
-export CROSS_COMPILE=~/tmp/arm-eabi-4.10/bin/arm-eabi-
+export CROSS_COMPILE=~/tmp/arm-eabi-4.9/bin/arm-eabi-
 export ENABLE_GRAPHITE=true
 make hammerhead_defconfig
 time make -j4 2>&1 | tee kernel.log
